42445993d812b95c098e2947258f09e68bfde732
marci1200 <113456793+Marci02@users.noreply.github.com>
2022-12-19 09:46:42 +0100

    Add files via upload

---
+++ Tesla/script.js
@@ -0,0 +1,9 @@
+function showImg(){
+    document.getElementById("img").src= "./img/m3white.jpg";
+}
+function showImg2(){
+    document.getElementById("img").src= "./img/m3blue.jpg";
+}
+function showImg3(){
+    document.getElementById("img").src= "./img/m3red.jpg";
+}
